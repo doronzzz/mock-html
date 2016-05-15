11c7304a1421fcc1dfbddff8885ad7081f2a2ab1
Doron Gour <doron.group@pontis.com>
2016-05-15 21:43:59 +0300

    handlbars ifcond

--- mods.js
+++ mods.js
@@ -1,3 +1,10 @@
+Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+  if(v1 === v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
 
 			var UserSession = (function(){
 
