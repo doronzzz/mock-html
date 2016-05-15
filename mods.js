aee2d43bbb9d7b150e568f091e8cea846d2a6860
Doron Gour <doron.group@pontis.com>
2016-05-15 21:50:24 +0300

    nocl

--- mods.js
+++ mods.js
@@ -1,11 +1,3 @@
-Handlebars.registerHelper('ifCond', function(v1, v2, options) {
-  if(v1 === v2) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
-});
-
-
 			var UserSession = (function(){
 
 					var public = {};
